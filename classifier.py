d7038909e9a4fda8a5a73aff90265191867bf49e
srinathtang123 <srinath.tangudu@gmail.com>
2021-09-18 19:38:12 +0530

    changed name of classifier to model in trainer self.clf->self.model

--- classifier.py
+++ classifier.py
@@ -107,20 +107,20 @@ class Trainer(object):
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         self.data_path = 'data/train/bq-results-20210825-203004-swh711l21gv2.csv'
         self.ratio=0.7 
-        self.train_batch_size=64
-        self.val_batch_size=64
+        self.train_batch_size=32
+        self.val_batch_size=32
         self.epochs = 10
         self.lr = 0.001
         self.log_dir='results/exp1/'
         self.model_save_path = 'results/exp1/'
-        self.clf = MyClassifier(self.device).to(self.device)
+        self.model = MyClassifier(self.device).to(self.device)
         #freze embeeder and tokenizer
-        for params in self.clf.embedder.parameters():
+        for params in self.model.embedder.parameters():
             params.requires_grad = False
         
-        # count_parameters(clf)
+        # count_parameters(model)
         self.criterion = nn.CrossEntropyLoss()
-        self.optimizer = Adam(self.clf.parameters(), lr=self.lr)
+        self.optimizer = Adam(self.model.parameters(), lr=self.lr)
         self.dataset = CreateDataset(data_path=self.data_path)
         self.train_loader,self.val_loader = split_train_val_dataloader(dataset=self.dataset, ratio=self.ratio,\
                                                      train_batch_size=self.train_batch_size, val_batch_size=self.val_batch_size)
@@ -129,13 +129,13 @@ class Trainer(object):
         self.val_step = 0
     def train_loop(self):
         for epoch in range(self.epochs):
-            self.clf.train()
+            self.model.train()
             self.train_loss_epoch = 0
             cnt = 0
             loop = tqdm(self.train_loader)
             for x,y in loop:
                 y = y.to(self.device)
-                y_hat = self.clf(x)
+                y_hat = self.model(x)
                 loss = self.criterion(y_hat,y.long())   
                 self.train_loss_epoch+=loss
                 cnt+=1
@@ -151,13 +151,13 @@ class Trainer(object):
                 # break
             self.train_loss_epoch/=cnt
             self.writer.add_scalar("epoch_loss/train", self.train_loss_epoch, global_step=epoch)
-            self.clf.eval()
+            self.model.eval()
             self.val_loss_epoc = 0
             cnt = 0
             loop = tqdm(self.val_loader)
             for x,y in loop:
                 y = y.to(self.device)
-                y_hat = self.clf(x)
+                y_hat = self.model(x)
                 loss = self.criterion(y_hat,y.long())
                 self.val_loss_epoc+=loss
                 cnt+=1
@@ -168,7 +168,7 @@ class Trainer(object):
                 # break
             self.val_loss_epoc/=cnt
             self.writer.add_scalar("epoch_loss/val", self.val_loss_epoc, global_step=epoch)
-            ckpt = {'epoch':epoch, 'state_dict':self.clf.state_dict(), 'optimizer':self.optimizer.state_dict(), 'loss':loss }
+            ckpt = {'epoch':epoch, 'state_dict':self.model.state_dict(), 'optimizer':self.optimizer.state_dict(), 'loss':loss }
             torch.save(ckpt, self.model_save_path+f'epoch:{epoch}-val_loss:{self.val_loss_epoc}.pth.tar')
             self.writer.add_hparams(
                     { "lr":self.lr, "batch_size":self.train_batch_size},
@@ -183,13 +183,13 @@ class Trainer(object):
         self.test_set = CreateDataset_test(self.test_data_path)
         self.test_loader = DataLoader(self.test_set, batch_size=self.test_batch_size)
         self.state_dict = torch.load(self.model_load_path)['state_dict']
-        self.clf.load_state_dict(self.state_dict)
+        self.model.load_state_dict(self.state_dict)
         self.labels_pred = []
         self.Ids = []
-        self.clf.eval()
+        self.model.eval()
         loop = tqdm(self.test_loader)
         for x,ids_batch in loop:
-            y_hat = self.clf(x)
+            y_hat = self.model(x)
             labels_pred_batch = torch.argmax(y_hat, dim=1)
             # self.labels_pred.append(labels_pred_batch.detach())
             for elm,id in zip(labels_pred_batch.cpu().numpy(), ids_batch.numpy()):
@@ -205,7 +205,7 @@ class Trainer(object):
 
 if __name__=='__main__':
     trainer = Trainer()
-    trainer.train_loop()
+    # trainer.train_loop()
     # trainer.test_loop('results/exp1/epoch:0-val_loss:0.6944313645362854.pth.tar')
     # with profile(activities=[ProfilerActivity.CUDA], record_shapes=True) as prof:
     #     with record_function("model_inference"):
